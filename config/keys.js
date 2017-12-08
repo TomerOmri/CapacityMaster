cb67bb5faa9f349e76eeb0e5ca54ed33f1c4e3ef
TomerOmri <tomer92@gmail.com>
2017-12-08 11:12:25 +0200

    add main routing

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
-
-
 if (process.env.NODE_ENV === 'production'){
     // run the production set of keys
-    module.exports = require('.prod');
+    module.exports = require('./prod');
 } else {
     // run dev
     module.exports = require('./dev');
