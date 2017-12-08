7c4d4ce0ad995789eb3d1f3a06203d6b2340d2dc
TomerOmri <tomer92@gmail.com>
2017-12-08 10:46:48 +0200

    add procces.env

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+
+
+if (process.env.NODE_ENV === 'production'){
+    // run the production set of keys
+    module.exports = require('.prod');
+} else {
+    // run dev
+    module.exports = require('./dev');
+}
