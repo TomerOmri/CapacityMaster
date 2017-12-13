59e5d9005c48089ba644564dfd8a83ba858cdadc
TomerOmri <tomer92@gmail.com>
2017-12-13 22:09:05 +0200

    add redux configure-first

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// create store ( Reducer , Initial state of my application , apply middleware code )
+const store = createStore( reducers , {} , applyMiddleware());
+
+
+ReactDOM.render( 
+    <Provider store={store}><App /></Provider>,
+    document.querySelector('#root') 
+); 
