4911e3953c87d6fe4eaba5fdaedae31739d89490
Tomer Omri <tomer.omri@sap.com>
2018-01-27 16:33:35 +0200

    add prod dep

--- index.js
+++ index.js
@@ -40,5 +40,15 @@ app.get(
 );
 
 require('./routes/authRoutes')(app);
+
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get( '*' , (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 var port = (process.env.PORT || 5000);
 app.listen(port);
